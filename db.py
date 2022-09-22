e654d252de5994eab8503e962a3942326ff6f58f
Sarishlitore <rishat.sabirovgm@gmail.com>
2022-09-22 23:29:27 +0300

    Docstring added

--- db.py
+++ db.py
@@ -2,21 +2,35 @@ from mysql.connector import connect, Error
 
 
 class DataBase:
-
+    """Class used to help work with db"""
     def __init__(self, host: str, user: str, password: str):
+        """
+        :param host: host's ip
+        :param user: username to log in to database
+        :param password: password to log in to database
+        """
         self.__host = host
         self.__user = user
         self.__password = password
         self.connection = None
 
     def __connect(self):
+        """
+        Try to connect to certain db in the given server
+        :return: None
+        """
         try:
             self.connection = connect(host=self.__host, user=self.__user, password=self.__password,
                                       database="cf19571_booksdb")
         except Error as e:
-            print(e)
+            print(f"Error: {e}")
 
     def save_book(self, name: str):
+        """
+        Save book name in db
+        :param name: book name
+        :return: None
+        """
         self.__connect()
         insert_book = f'INSERT INTO books (book_name) VALUES("{name}")'
         self.connection.cursor().execute(insert_book)
@@ -24,6 +38,11 @@ class DataBase:
         self.connection.disconnect()
 
     def delete_book(self, name: str):
+        """
+        Delete book from db by name
+        :param name: book name
+        :return: None
+        """
         self.__connect()
         delete_book = f'DELETE FROM books WHERE book_name = "{name}"'
         self.connection.cursor().execute(delete_book)
@@ -31,7 +50,12 @@ class DataBase:
         self.connection.disconnect()
 
     def show_books(self):
+        """
+        :return: list of saved books
+        """
+        print(self.connection, 1)
         self.__connect()
+        print(self.connection, 2)
         show_books = f'SELECT book_name FROM books'
         with self.connection.cursor() as cursor:
             cursor.execute(show_books)
--- telegram_bot.py
+++ telegram_bot.py
@@ -1,3 +1,5 @@
+"""Bot that shows updates for books from @bookfloor_bot"""
+
 from telebot import TeleBot, types
 
 import credentials
@@ -10,6 +12,12 @@ bot = TeleBot(credentials.bot_token)
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
+    """
+    Show menu of bot's functionality: save book in db, delete book from db,
+    show saved books with their updates in @bookfloor3_bot
+    :param message: handled message
+    :return: None
+    """
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     save_button = types.KeyboardButton("Сохранить книгу")
     delete_button = types.KeyboardButton("Удалить книгу")
@@ -22,16 +30,31 @@ def start_message(message):
 
 @bot.message_handler(commands=['chat_id'])
 def chat_id_message(message):
+    """
+    Send chat_id
+    :param message: handled message
+    :return: None
+    """
     bot.send_message(message.chat.id, message.chat.id)
 
 
 @bot.message_handler(commands=['help'])
 def help_message(message):
+    """
+    Send list of available commands
+    :param message: handled message
+    :return: None
+    """
     bot.send_message(message.chat.id, show_available_commands())
 
 
 @bot.message_handler(content_types='text')
 def message_reply(message):
+    """
+    Handle commands from menu
+    :param message: handled message
+    :return: None
+    """
     if message.text == "Сохранить книгу":
         bot.register_next_step_handler(message, save_book)
     elif message.text == "Удалить книгу":
@@ -43,6 +66,11 @@ def message_reply(message):
 
 
 def save_book(message):
+    """
+    Save book's name from db
+    :param message: handled message
+    :return: None
+    """
     try:
         book_name = message.text
         db.save_book(book_name)
@@ -51,6 +79,11 @@ def save_book(message):
 
 
 def delete_book(message):
+    """
+    Delete book's name from db
+    :param message: handled message
+    :return: None
+    """
     try:
         book_name = message.text
         db.delete_book(book_name)
@@ -59,6 +92,9 @@ def delete_book(message):
 
 
 def show_saved_books() -> str:
+    """
+    :return: list of books names that saved in db
+    """
     text = "Сохраненные книги:\n"
     books = db.show_books()
     for book in books:
@@ -67,6 +103,9 @@ def show_saved_books() -> str:
 
 
 def show_available_commands() -> str:
+    """
+    :return: list of available commands in clickable state
+    """
     commands = ['start', 'help', 'chat_id']
     return "Доступные комманды /" + " /".join(commands)
 
